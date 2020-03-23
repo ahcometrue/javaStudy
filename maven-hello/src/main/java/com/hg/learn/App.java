3d54255a7c3b5ba7289937e4f923d0f6db1dbfa8
hanguang <hanguang@pyyx.com>
2020-03-23 18:24:13 +0800

    使用maven并添加依赖

---
+++ maven-hello/src/main/java/com/hg/learn/App.java
@@ -0,0 +1,20 @@
+package com.hg.learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("start application...");
+        for (int i = 1; i <= 10; i++) {
+            logger.warn("begin task {}...", i);
+        }
+        logger.info("done.");
+        System.out.println("Hello World!");
+    }
+}
